9c09f22e026909744d4c0cd79613c013496ce1a6
goober37 <tillmanwinnerreilly@gmail.com>
2025-06-05 13:55:09 -0800

    removed filesystem stuff (error is thrown when space is held -> program stops -> bug is a feature

--- deskbuddy.py
+++ deskbuddy.py
@@ -16,7 +16,7 @@ bigx,bigy = pygame.display.get_desktop_sizes()[0]
 print(bigx, bigy)
 screenborders = pygame.Rect(0,0,bigx,bigy)
 ghop = pygame.Rect(bigx/2,bigy/2,263,187)
-ghov = [5,5]
+ghov = [20,20]
 
 if not pygame.mixer.get_init():
     pygame.mixer.init()
@@ -24,8 +24,11 @@ pygame.mixer.music.load("spooktune.mp3")
 pygame.mixer.music.play(-1) 
 
 while running:
-    rendghost.makeWind(ghop[0], ghop[1])
+    #move the ghost window
     ghop, ghov = dvd.bounceInner(screenborders, ghop, ghov)
+    #make the ghost window
+    rendghost.makeWind(ghop[0], ghop[1])
+
     with open("running.txt", "r", encoding="utf-8") as f:
         running = f
     
