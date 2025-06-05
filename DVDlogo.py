bf3a33fc0e29642974ff79d87c3c17176306a391
goober37 <tillmanwinnerreilly@gmail.com>
2025-06-05 12:14:11 -0800

    bounceInner now uses a list for velocity instead of two variables

--- DVDlogo.py
+++ DVDlogo.py
@@ -7,21 +7,22 @@ screen = pg.display.set_mode()
 clock = pg.time.Clock()
 boundx, boundy = pg.display.get_window_size()
 running = True
-textvelx, textvely = 10,10
+textvel = [10,10]
+debug = "yes"
 
 #math the font
 font = pg.font.Font(None, 64)
 text = font.render("DVD", True, (255, 255, 255))
 textpos = text.get_rect(x=1, y=1)
 
-def bounceInner(bound=pg.Rect(0,0,100,100), inner=pg.Rect(0,0,10,10),velx=0,vely=0):
+def bounceInner(bound=pg.Rect(0,0,100,100), inner=pg.Rect(0,0,10,10),vel=[10,10]):
     if inner[0] <= bound[0] or inner[0]+inner[2] >= bound[0]+bound[2]:
-        velx *= -1
-        inner = inner.move(velx , 0)
+        vel[0] *= -1
+        inner = inner.move(vel[0] , 0)
     if inner[1] <= bound[1] or inner[1] + inner[3] >= bound[1] + bound[3]:
-        vely *= -1
-        inner = inner.move(0 , vely)
-    return velx, vely, inner
+        vel[1] *= -1
+        inner = inner.move(0 , vel[1])
+    return inner, vel
 
 while running:
     # poll for events
@@ -35,13 +36,14 @@ while running:
     # RENDER YOUR GAME HERE
     if pg.font:
         #move font
-        textpos = textpos.move(textvelx,textvely)
-        textvelx, textvely, textpos = bounceInner([0,0,boundx,boundy],textpos, textvelx, textvely)
+        textpos = textpos.move(textvel[0],textvel[1])
+        textpos, textvel = bounceInner([0,0,boundx,boundy],textpos, textvel)
         #render font
         screen.blit(text, textpos)
     #draw debug stuff
-    pg.draw.rect(screen,(255,0,0),textpos, width=3)
-    pg.draw.circle(screen,"green",(textpos[0],textpos[1]),3)
+    if debug == "yes":
+        pg.draw.rect(screen,(255,0,0),textpos, width=3)
+        pg.draw.circle(screen,"green",(textpos[0],textpos[1]),3)
     #cleanup
     pg.display.flip()
     clock.tick(30)
--- deskbuddy.py
+++ deskbuddy.py
@@ -4,6 +4,7 @@ import pygame
 import os
 import rendghost
 import numpy as np
+import DVDlogo as dvd
 
 
 # pygame setup
@@ -12,7 +13,8 @@ screen = pygame.display.set_mode((1,1))
 clock = pygame.time.Clock()
 running = True
 bigx,bigy = pygame.display.get_desktop_sizes()[0]
-ghox, ghoy = bigx/2,bigy/2
+ghop = pygame.rect(bigx/2,bigy/2,263,187)
+ghov = [5,5]
 
 if not pygame.mixer.get_init():
     pygame.mixer.init()
@@ -20,14 +22,11 @@ pygame.mixer.music.load("spooktune.mp3")
 pygame.mixer.music.play(-1) 
 
 while running:
-    rendghost.makeWind(ghox, ghoy)
-
+    rendghost.makeWind(ghop[0], ghop[1])
+    ghop, ghov = dvd.bounceInner([pygame.rect(0,0,bigx,bigy)], ghop, ghov)
     with open("running.txt", "r", encoding="utf-8") as f:
         running = f
-
-    ghox = ghox + np.random.randint(-30,30)
-    ghoy = ghoy + np.random.randint(-30,30)
-
+    
     clock.tick(30)  # limits FPS to 30
 
 pygame.quit()
