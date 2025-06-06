a04363b14f34c6f8f457b941f621c887d5e5694e
goober37 <tillmanwinnerreilly@gmail.com>
2025-06-06 08:40:30 -0800

    fixed "no bump on x edges" bug

--- deskbuddy.py
+++ deskbuddy.py
@@ -14,19 +14,21 @@ clock = pygame.time.Clock()
 running = True
 bigx,bigy = pygame.display.get_desktop_sizes()[0]
 screenborders = pygame.Rect(0,0,bigx,bigy) #screen as a rect
-ghop = pygame.Rect(0, 0, 263, 187) #ghost position as a rect
+ghop = pygame.Rect((2*bigx/3), (2*bigy/3), 263, 187) #ghost position as a rect
 ghov = [30,30] #ghost velocity as a rect
 
 while running:
     #move the ghost window
     ghop = pygame.Rect(ghop[0]+ghov[0],ghop[1]+ghov[1],ghop[2],ghop[3])
-    ghop, ghov, bump = dvd.bounceInner(screenborders, ghop, ghov)
+    ghop, ghov, bump, state = dvd.bounceInner(screenborders, ghop, ghov)
+    print(state)
     #make the ghost window (he might be hurt)
     if bump:
         rendghost.makeWind(ghop[0], ghop[1],"ghosthurt.png")
         pygame.mixer.init()
         pygame.mixer.music.load("spooktune.mp3")
         pygame.mixer.music.play(-1)
+        print("owchies!")
     else:
         rendghost.makeWind(ghop[0], ghop[1])
     clock.tick(30)  # limits FPS to 30
